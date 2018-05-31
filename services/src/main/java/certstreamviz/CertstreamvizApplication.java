9b6987e2e6b9a76c0de938adbac06caf62654301
Edgardo Vega <1819243+vegaed@users.noreply.github.com>
2018-05-31 11:08:22 -0400

    Initial Commit with starter projects
    Two subprojects web and services
    Used angular cli to create starter web app
    Used spring initializr for service
    Using gradle for building
    MIT License

---
+++ services/src/main/java/certstreamviz/CertstreamvizApplication.java
@@ -0,0 +1,12 @@
+package certstreamviz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertstreamvizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertstreamvizApplication.class, args);
+	}
+}
---
+++ web/src/app/material.module.ts
@@ -0,0 +1,5 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({})
+export class MaterialModule {}
