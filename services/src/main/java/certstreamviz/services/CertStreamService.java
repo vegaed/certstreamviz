57570f7e3908a756e3febb754b523909d02f1221
Edgardo Vega <1819243+vegaed@users.noreply.github.com>
2018-06-01 20:56:51 -0400

    cleanup imports

--- services/src/main/java/certstreamviz/services/CertStreamService.java
+++ services/src/main/java/certstreamviz/services/CertStreamService.java
@@ -7,8 +7,6 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.stereotype.Service;
 
 import certstreamviz.models.Coordinate;
