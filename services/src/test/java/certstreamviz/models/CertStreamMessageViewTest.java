c120e77c5914ed6e95475ae11ac4d8552183feb1
Edgardo Vega <1819243+vegaed@users.noreply.github.com>
2018-06-03 13:35:53 -0400

    test for CertSeamMessageView

---
+++ services/src/test/java/certstreamviz/models/CertStreamMessageViewTest.java
@@ -0,0 +1,68 @@
+package certstreamviz.models;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import static org.hamcrest.Matchers.allOf;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.json.JSONException;
+import org.junit.Test;
+import org.skyscreamer.jsonassert.JSONAssert;
+
+import certstreamviz.models.certstreammessage.CertStreamMessage;
+
+public class CertStreamMessageViewTest {
+
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    @Test
+    public void testSerialization() throws IOException, JSONException {
+
+        CertStreamMessageView csmv = new CertStreamMessageView();
+        csmv.coordinate = new Coordinate(0, 0);
+        csmv.issuer = "issuer";
+        csmv.source = "source";
+        csmv.cn = "cn";
+
+        String json = objectMapper.writeValueAsString(csmv);
+
+        String expectedJson = "{cn:\"cn\", source:\"source\", issuer:\"issuer\", coordinate:{latitude:0,longitude:0} }";
+        JSONAssert.assertEquals(expectedJson, json, true);
+    }
+
+    @Test
+    public void testSerializationWithMissingCoordinate() throws IOException, JSONException {
+
+        CertStreamMessageView csmv = new CertStreamMessageView();
+        csmv.issuer = "issuer";
+        csmv.source = "source";
+        csmv.cn = "cn";
+
+        String json = objectMapper.writeValueAsString(csmv);
+
+        String expectedJson = "{cn:\"cn\", source:\"source\", issuer:\"issuer\", coordinate:null }";
+        JSONAssert.assertEquals(expectedJson, json, true);
+    }
+
+    @Test
+    public void testConverterFunction() throws IOException {
+        InputStream is = this.getClass().getResourceAsStream("/certstreammessage");
+        // TODO: get spring object mapper
+        ObjectMapper om = new ObjectMapper();
+        CertStreamMessage msg = om.readValue(is, CertStreamMessage.class);
+
+        CertStreamMessageView view = CertStreamMessageView.convertMsgToView.apply(msg);
+        CertStreamMessageView expected = new CertStreamMessageView();
+        expected.cn = "wexsav.com";
+        expected.issuer = "Let's Encrypt Authority X3";
+        expected.source = "Google 'Argon2018' log";
+        assertThat(view, is(expected));
+    }
+
+}
